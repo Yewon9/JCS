042ae2ae4797e55bad717b47063e7d4b8d86fec8
laura <jyw7104@sch.ac.kr>
2022-01-14 11:25:40 +0900

    0114

--- Models/resnet.py
+++ Models/resnet.py
@@ -16,12 +16,14 @@ model_urls = {
 
 from torch.nn import BatchNorm2d 
 
+#nn.Conv2d 결과 리턴
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
 
 
+#nn.Conv2d 결과 리턴
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
@@ -40,6 +42,8 @@ class BasicBlock(nn.Module):
         self.downsample = downsample
         self.stride = stride
 
+    #out 리턴
+    #
     def forward(self, x):
         identity = x
 
